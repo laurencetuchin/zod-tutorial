9e463f385febf03faf4f75fa4e0be4579240b5ca
laurencetuchin <laurencetuchin@gmail.com>
2023-06-13 10:54:59 +1000

    Add solution for e04

--- src/04-infer.problem.ts
+++ src/04-infer.problem.ts
@@ -3,16 +3,18 @@
 import { z } from "zod";
 
 const StarWarsPerson = z.object({
-  name: z.string(),
+	name: z.string(),
 });
 
 const StarWarsPeopleResults = z.object({
-  results: z.array(StarWarsPerson),
+	results: z.array(StarWarsPerson),
 });
 
-const logStarWarsPeopleResults = (data: unknown) => {
-  //                                    ^ 🕵️‍♂️
-  data.results.map((person) => {
-    console.log(person.name);
-  });
+const logStarWarsPeopleResults = (
+	data: z.infer<typeof StarWarsPeopleResults>
+) => {
+	//                                    ^ 🕵️‍♂️
+	data.results.map((person) => {
+		console.log(person.name);
+	});
 };
