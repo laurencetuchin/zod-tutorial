0bd300ff61b5f70c6d37cc450843b0f963a0a052
laurencetuchin <laurencetuchin@gmail.com>
2023-06-13 14:58:57 +1000

    Add solution for e14

--- src/14-generics.problem.ts
+++ src/14-generics.problem.ts
@@ -4,25 +4,28 @@ import { it } from "vitest";
 import { z } from "zod";
 import { Equal, Expect } from "./helpers/type-utils";
 
-const genericFetch = (url: string, schema: z.ZodSchema) => {
-  //                 ^ 🕵️‍♂️
-  return fetch(url)
-    .then((res) => res.json())
-    .then((result) => schema.parse(result));
+const genericFetch = <ZSchema extends z.ZodSchema>(
+	url: string,
+	schema: ZSchema
+): Promise<z.infer<ZSchema>> => {
+	//                 ^ 🕵️‍♂️
+	return fetch(url)
+		.then((res) => res.json())
+		.then((result) => schema.parse(result));
 };
 
 // TESTS
 
 it("Should fetch from the Star Wars API", async () => {
-  const result = await genericFetch(
-    "https://www.totaltypescript.com/swapi/people/1.json",
-    z.object({
-      name: z.string(),
-    }),
-  );
+	const result = await genericFetch(
+		"https://www.totaltypescript.com/swapi/people/1.json",
+		z.object({
+			name: z.string(),
+		})
+	);
 
-  type cases = [
-    // Result should equal { name: string }, not any
-    Expect<Equal<typeof result, { name: string }>>,
-  ];
+	type cases = [
+		// Result should equal { name: string }, not any
+		Expect<Equal<typeof result, { name: string }>>
+	];
 });
