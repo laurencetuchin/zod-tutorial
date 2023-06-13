60950f086710779bde339021b64a01db04b96ee4
laurencetuchin <laurencetuchin@gmail.com>
2023-06-13 11:47:57 +1000

    Add solution for e09

--- src/09-composing-objects.problem.ts
+++ src/09-composing-objects.problem.ts
@@ -1,31 +1,32 @@
 import { z } from "zod";
 import { Equal, Expect } from "./helpers/type-utils";
 
+const Id = z.string().uuid();
 /**
  * 🕵️‍♂️ Refactor this code below to reduce the duplication,
  * while also making sure the cases don't go red!
  */
 
 const User = z.object({
-  id: z.string().uuid(),
-  name: z.string(),
+	id: Id,
+	name: z.string(),
 });
 
 const Post = z.object({
-  id: z.string().uuid(),
-  title: z.string(),
-  body: z.string(),
+	id: Id,
+	title: z.string(),
+	body: z.string(),
 });
 
 const Comment = z.object({
-  id: z.string().uuid(),
-  text: z.string(),
+	id: Id,
+	text: z.string(),
 });
 
 type cases = [
-  Expect<Equal<z.infer<typeof Comment>, { id: string; text: string }>>,
-  Expect<
-    Equal<z.infer<typeof Post>, { id: string; title: string; body: string }>
-  >,
-  Expect<Equal<z.infer<typeof User>, { id: string; name: string }>>,
+	Expect<Equal<z.infer<typeof Comment>, { id: string; text: string }>>,
+	Expect<
+		Equal<z.infer<typeof Post>, { id: string; title: string; body: string }>
+	>,
+	Expect<Equal<z.infer<typeof User>, { id: string; name: string }>>
 ];
